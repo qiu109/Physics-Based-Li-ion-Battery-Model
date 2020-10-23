bf3d97d0715c02b865a7771e8a8eab00665fff47
ecekurt <56387994+ecekurt@users.noreply.github.com>
2020-10-23 10:14:12 +0300

    Update ode_cont1.m

--- ode_cont1.m
+++ ode_cont1.m
@@ -1,5 +1,9 @@
-function [value, isterminal, direction] = ode_cont1(t, U_n)
-value= U_n(49)- (0.0001);
+function [value, isterminal, direction] = ode_cont1(t, U_n,p)
+
+ min_conc=10;
+ 
+value= U_n(49)- (min_conc);
 isterminal=1;
 direction  = 0;
+
 end
